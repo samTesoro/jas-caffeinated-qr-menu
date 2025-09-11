6a5c823c723b05bfa8989e29ce2261d43d6e87fd
dambaculio <dambaculio@addu.edu.ph>
2025-09-11 19:01:21 +0800

    history, and notif

--- app/admin/history/page.tsx
+++ app/admin/history/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 import Taskbar from "@/components/admin/taskbar-admin";
 import DashboardHeader from "@/components/ui/header";
+import OrderHistory from "@/components/admin/order-history";
 
 export default function HistoryPage() {
   return (
     <div className="min-h-screen bg-[#ebebeb]">
       <DashboardHeader showBack={false} />
+      <OrderHistory />
       <Taskbar />
     </div>
   );
--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 import Taskbar from "@/components/admin/taskbar-admin";
 import DashboardHeader from "@/components/ui/header";
+import OrderNotification from "@/components/admin/order-notification";
 
 export default function OrdersPage() {
   return (
     <div className="min-h-screen bg-[#ebebeb]">
       <DashboardHeader showBack={false} />
+      <div className="flex-1 px-5 pb-20">
+        <OrderNotification />
+      </div>
       <Taskbar />
     </div>
   );
